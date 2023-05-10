ee75ccabb760361d8ffce15f38d3d94fb5691db9
WCeasar <285665110@qq.com>
2023-05-10 20:04:22 +0800

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const a = '123'
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
